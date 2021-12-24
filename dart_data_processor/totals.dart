6788c2e9eef505a85b4480815a09dbf0e030270f
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-24 23:46:14 +0000

    processing csv file in dart

--- dart_data_processor/totals.dart
+++ dart_data_processor/totals.dart
@@ -1,3 +1,37 @@
-void main(){
-  
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print(arguments);
+
+  if (arguments.isEmpty) {
+    print('Usage: dart totals.dart <inputFile.csv>');
+    exit(1);
+  }
+
+  final inputFile = arguments.first;
+  print(inputFile);
+
+  final lines = File(inputFile).readAsLinesSync();
+
+  final totalDurationByTag = <String, double>{};
+
+  lines.removeAt(0);
+  for (var line in lines) {
+    // print(line);
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null) {
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] = previousTotal + duration;
+    }
+  }
+  for (var entry in totalDurationByTag.entries) {
+    final durationFormatted = entry.value.toStringAsFixed(1);
+    final tag = entry.key == '' ? 'Unallocated' : entry.key;
+    print('$tag: ${durationFormatted}h');
+  }
 }
