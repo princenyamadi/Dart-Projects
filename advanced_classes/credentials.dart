22b5a7ce33c6bbf4f549cf21bc989a0b1416a66f
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-26 20:18:35 +0000

    copying objects with copyWith

---
+++ advanced_classes/credentials.dart
@@ -0,0 +1,28 @@
+class Credentials {
+  const Credentials({this.email = '', this.password = ''});
+
+  final String email;
+  final String password;
+
+  Credentials copyWith({
+    String? email,
+    String? password,
+  }) {
+    return Credentials(
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+
+  @override
+  String toString() => 'Credentials($email, $password)';
+}
+
+void main() {
+  const credentials = Credentials();
+
+  final updated1 = credentials.copyWith(email: 'me@example.com');
+  print(updated1);
+  final updated2 = credentials.copyWith(password: 'tooeasy');
+  print(updated2);
+}
