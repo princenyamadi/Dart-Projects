756bdb7f86c8de89ebd4153c4696c75d1ed7e4e0
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-31 23:29:27 +0000

    stream methods

--- async_ops/streams2.dart
+++ async_ops/streams2.dart
@@ -1,9 +1,17 @@
-void main() {
-  Stream.fromIterable([1, 2, 3]);
-  Stream.value(10);
-  Stream.error(Exception('Something went wrong'));
-  Stream.empty();
-  Stream.fromFuture(Future.delayed(Duration(seconds: 1), () => 42));
-  Stream.periodic(Duration(seconds: 1), (index) => index);
-  
+Future<void> main() async {
+  // Stream.fromIterable([1, 2, 3]);
+  // Stream.value(10);
+  // Stream.error(Exception('Something went wrong'));
+  // Stream.empty();
+  // Stream.fromFuture(Future.delayed(Duration(seconds: 1), () => 42));
+  // Stream.periodic(Duration(seconds: 1), (index) => index);
+
+  final stream = Stream.fromIterable([1, 2, 3]);
+  // final value = await stream.first;
+  // print(value);
+
+  await stream.forEach((element) => print(element));
+  final doubles = stream.map((value) => value * 2).where((value) => value > 3);
+  print(doubles);
+  await doubles.forEach(print);
 }
