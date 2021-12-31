68ced44d8be0b62f0dcf214c2d0d3286326072ed
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-31 22:43:21 +0000

    futures, then, catchError, whenComplete

---
+++ async_ops/futures.dart
@@ -0,0 +1,14 @@
+Future<String> fetchUserOrder() => Future.delayed(
+      Duration(seconds: 2),
+      () => throw Exception('Out of milk'),
+    );
+
+void main() {
+  print('Program started');
+  fetchUserOrder()
+      .then((order) => print('Order is ready: $order'))
+      .catchError((error) => print(error))
+      .whenComplete(() => print('done'));
+
+  
+}
