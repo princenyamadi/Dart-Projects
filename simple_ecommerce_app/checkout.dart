1919f1e88447fdf5ca1e098645ea2f4ef275d6be
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-31 19:34:19 +0000

    command line flow logic

--- simple_ecommerce_app/checkout.dart
+++ simple_ecommerce_app/checkout.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Product {
   const Product({required this.id, required this.name, required this.price});
 
@@ -15,7 +17,36 @@ class Item {
   double get price => quantity * product.price;
 }
 
-
-class Cart{
+class Cart {
   // TODO: Items collection
+}
+
+// loop
+//  prompt: view cart / add item / checkout
+//  if selection == add item
+//    choose a product
+//    add it to the cart
+//    print cart
+//   else if selection == view cart
+//    print cart
+//   else if selection == checkout
+//    do checkout
+//    exit
+// end
+
+void main() {
+  while (true) {
+    stdout.write(
+        'What do you want to do? (v)iew items, (a)dd item, (c)hecjout: ');
+
+    final line = stdin.readLineSync();
+    if(line == 'a'){
+      // TODO: Implement add logic
+    }else if(line == 'v'){
+      // TODO: Implement view logic
+    }else if(line == 'c'){
+      // TODO: Implement checkout logic
+
+    }
+  }
 }
