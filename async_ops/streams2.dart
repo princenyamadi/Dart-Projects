c3a231d8460e95535433d1208d83212d2a85a0be
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-31 23:23:40 +0000

    Stream constructors

---
+++ async_ops/streams2.dart
@@ -0,0 +1,9 @@
+void main() {
+  Stream.fromIterable([1, 2, 3]);
+  Stream.value(10);
+  Stream.error(Exception('Something went wrong'));
+  Stream.empty();
+  Stream.fromFuture(Future.delayed(Duration(seconds: 1), () => 42));
+  Stream.periodic(Duration(seconds: 1), (index) => index);
+  
+}
