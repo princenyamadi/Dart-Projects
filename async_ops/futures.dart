40b0390a036cc6a99acd02c8d2e87c52b2b34c02
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-31 23:01:46 +0000

    future.value, future.error

--- async_ops/futures.dart
+++ async_ops/futures.dart
@@ -3,12 +3,29 @@ Future<String> fetchUserOrder() => Future.delayed(
       () => throw Exception('Out of milk'),
     );
 
-void main() {
-  print('Program started');
-  fetchUserOrder()
-      .then((order) => print('Order is ready: $order'))
-      .catchError((error) => print(error))
-      .whenComplete(() => print('done'));
+Future<String> fetchUserOrder2() => Future.value('Expresso');
+Future<String> fetchUserOrder3() => Future.error(Exception('Out of Milk'));
 
-  
+
+void main() async {
+  print('Program started');
+  // fetchUserOrder()
+  //     .then((order) => print('Order is ready: $order'))
+  //     .catchError((error) => print(error))
+  //     .whenComplete(() => print('done'));
+
+  try {
+    final order = await fetchUserOrder();
+    print(order);
+
+    final order2 = await fetchUserOrder2();
+    print(order2);
+
+    final order3 = await fetchUserOrder3();
+    print(order3);
+
+
+  } catch (e) {
+    print(e);
+  }
 }
