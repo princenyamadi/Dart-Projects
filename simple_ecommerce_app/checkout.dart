5141d6d04fb120d2f2705ce84d8d7c7eaaff02e4
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-31 20:09:17 +0000

    adding items to cart

--- simple_ecommerce_app/checkout.dart
+++ simple_ecommerce_app/checkout.dart
@@ -6,6 +6,9 @@ class Product {
   final int id;
   final String name;
   final double price;
+
+  String get displayName => '($initial)${name.substring(1)}: \$$price';
+  String get initial => name.substring(0, 1);
 }
 
 class Item {
@@ -15,11 +18,46 @@ class Item {
   final int quantity;
 
   double get price => quantity * product.price;
+  @override
+  String toString() => '$quantity x ${product.name}: \$$price ';
 }
 
 class Cart {
-  // TODO: Items collection
+  final Map<int, Item> _items = {};
+
+  void addProduct(Product product) {
+    final item = _items[product.id];
+    if (item == null) {
+      _items[product.id] = Item(product: product, quantity: 1);
+    } else {
+      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
+    }
+  }
+
+  double total() => _items.values
+      .map((item) => item.price)
+      .reduce((value, element) => value + element);
+
+  @override
+  String toString() {
+    if (_items.isEmpty) {
+      return 'Card is empty';
+    }
+
+    final itemizedList =
+        _items.values.map((item) => item.toString()).join('\n');
+    return '------\n$itemizedList\nTotal: \$${total()}\n------';
+  }
 }
+
+const allProducts = [
+  Product(id: 1, name: 'apples', price: 1.60),
+  Product(id: 2, name: 'bananas', price: 0.70),
+  Product(id: 3, name: 'courgettes', price: 1.00),
+  Product(id: 4, name: 'grapes', price: 2.00),
+  Product(id: 5, name: 'mushrooms', price: 0.80),
+  Product(id: 6, name: 'potatoes', price: 1.50),
+];
 
 // loop
 //  prompt: view cart / add item / checkout
@@ -35,18 +73,38 @@ class Cart {
 // end
 
 void main() {
+  final cart = Cart();
   while (true) {
     stdout.write(
         'What do you want to do? (v)iew items, (a)dd item, (c)hecjout: ');
 
     final line = stdin.readLineSync();
-    if(line == 'a'){
-      // TODO: Implement add logic
-    }else if(line == 'v'){
-      // TODO: Implement view logic
-    }else if(line == 'c'){
+    if (line == 'a') {
+      final product = chooseProduct();
+      if (product != null) {
+        cart.addProduct(product);
+        print(cart);
+      }
+    } else if (line == 'v') {
+      print(cart);
+    } else if (line == 'c') {
       // TODO: Implement checkout logic
 
     }
   }
+}
+
+Product? chooseProduct() {
+  final productsList =
+      allProducts.map((product) => product.displayName).join('\n');
+
+  stdout.write('Available products: \n$productsList\nYour choice: ');
+  final line = stdin.readLineSync();
+  for (var product in allProducts) {
+    if (product.initial == line) {
+      return product;
+    }
+  }
+  print('Not found');
+  return null;
 }
