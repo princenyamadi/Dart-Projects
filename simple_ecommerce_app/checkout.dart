1dcfaeaa65f7c788c2e97bc20790f859ca263342
princenyamadi <princenyamadi.pn@gmail.com>
2021-12-31 20:32:18 +0000

    checkout functionality

--- simple_ecommerce_app/checkout.dart
+++ simple_ecommerce_app/checkout.dart
@@ -34,6 +34,8 @@ class Cart {
     }
   }
 
+  bool get isEmpty => _items.isEmpty;
+
   double total() => _items.values
       .map((item) => item.price)
       .reduce((value, element) => value + element);
@@ -88,8 +90,9 @@ void main() {
     } else if (line == 'v') {
       print(cart);
     } else if (line == 'c') {
-      // TODO: Implement checkout logic
-
+      if (checkOut(cart)) {
+        break;
+      }
     }
   }
 }
@@ -107,4 +110,32 @@ Product? chooseProduct() {
   }
   print('Not found');
   return null;
+}
+
+bool checkOut(Cart cart) {
+  if (cart.isEmpty) {
+    print('Cart is empty');
+    return false;
+  }
+  final total = cart.total();
+  print('Total: \$$total');
+  stdout.write("Payment in cash: ");
+  final line = stdin.readLineSync();
+
+  if (line == null || line.isEmpty) {
+    return false;
+  }
+  final paid = double.tryParse(line);
+  if (paid == null) {
+    return false;
+  }
+
+  if (paid >= total) {
+    final change = paid - total;
+    print('Change: \$${change.toStringAsFixed(2)}');
+    return true;
+  } else {
+    print('Not enough cash.');
+    return false;
+  }
 }
